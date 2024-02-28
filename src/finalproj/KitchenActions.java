4d2ad29bc3f57227b31b361e2c19c9ae422c3429
stucholski <116388996+stucholski@users.noreply.github.com>
2024-02-28 07:23:08 -0600

    Delete src/finalproj/KitchenActions.java

--- src/finalproj/KitchenActions.java
+++
@@ -1,9 +0,0 @@
-package finalproj;
-
-// Interface for the Kitchen
-public interface KitchenActions {
-    void enterKitchen();
-    void exploreKitchen();
-    void interactWithObjects();
-    void prepareMeal();
-}
