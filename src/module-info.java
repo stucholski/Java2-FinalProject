cb1e5ec8b048d4fd4b63a7ac6e06b3fb22666835
Moises Alcantara <20762255@nwtc.edu>
2024-03-05 15:06:37 -0600

    Added logic to prevent chef from cooking in the dining room

--- src/module-info.java
+++ src/module-info.java
@@ -9,7 +9,6 @@ module Final_Project1 {
     requires org.apache.logging.log4j;
     requires jdk.compiler;
     requires java.sql;
-    //requires derby;
     requires java.naming;
     requires derby;
 }
